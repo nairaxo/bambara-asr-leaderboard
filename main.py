87a2b5d716e8e6d7ac56bafaca160e23a111523d
HF Space <naira.abdoumohamed@gmail.com>
2026-02-16 12:17:39 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from bambara-asr-leaderboard!")
+
+
+if __name__ == "__main__":
+    main()
--- space/settings/models.py
+++ space/settings/models.py
@@ -6,12 +6,12 @@ class Settings(BaseSettings):
     dataset_url: str      = Field(validation_alias="DATASET_URL", default="sudoping01/bam-asr-benchmark") # this is where we have the label this is supposed to not be open (closed)
     dataset_config: str   = Field(validation_alias="DATASET_CONFIG", default="eval")
     split_name: str       = Field(validation_alias="SPLIT_NAME", default="eval")
-    leaderboard_file: str = Field(validation_alias="LEADERBOARD_FILE", default="leaderboard.csv")
+    leaderboard_file: str = Field(validation_alias="LEADERBOARD_FILE", default="../leaderboard.csv")
     logo_path: str        = Field(validation_alias="LOGO_PATH", default="assets/images/bambara-logo.png")
     hf_token: str         = Field(validation_alias="HF_TOKEN", default=os.environ.get("HG_TOKEN"))
-    github_user: str      = Field(validation_alias="GITHUB_USER", default="sudoping01")
-    github_repo: str      = Field(validation_alias="GITHUB_REPO", default="MALIBA-AI/bambara-asr-leaderboard")
-    github_email: str     = Field(validation_alias="GITHUB_EMAIL", default="sudoping01@gmail.com")
+    github_user: str      = Field(validation_alias="GITHUB_USER", default="nairaxo")
+    github_repo: str      = Field(validation_alias="GITHUB_REPO", default="nairaxo/bambara-asr-leaderboard")
+    github_email: str     = Field(validation_alias="GITHUB_EMAIL", default="naira.abdoumohamed@gmail.com")
     github_token: str     = Field(validation_alias="GITHUB_TOKEN", default=os.environ.get("GITHUB_TOKEN"))
 
 
